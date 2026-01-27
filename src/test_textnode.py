1ae34f4fd6c24d275e80a09c0fae7229eb0fba40
aaronmkwong <amk841@gmail.com>
2026-01-27 13:24:14 -0700

    add function text_node_to_html_node and unit test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,22 +2,28 @@
 
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
 	def test_eq(self):
-        	node = TextNode("This is a text node", TextType.BOLD)
-        	node2 = TextNode("This is a text node", TextType.BOLD)
-        	self.assertEqual(node, node2)
+		node = TextNode("This is a text node", TextType.BOLD)
+		node2 = TextNode("This is a text node", TextType.BOLD)
+		self.assertEqual(node, node2)
 
 	def test_url_is_none(self):
 		node = TextNode("This is a text node", TextType.LINK)
 		self.assertIsNone(node.url)
 	
 	def test_not_eq(self):
-   		node = TextNode("This is a text node", TextType.BOLD)
-   		node2 = TextNode("This is a text node", TextType.LINK)
-   		self.assertNotEqual(node, node2)
+		node = TextNode("This is a text node", TextType.BOLD)
+		node2 = TextNode("This is a text node", TextType.LINK)
+		self.assertNotEqual(node, node2)
+
+	def test_text(self):
+		node = TextNode("This is a text node", TextType.TEXT)
+		html_node = text_node_to_html_node(node)
+		self.assertEqual(html_node.tag, None)
+		self.assertEqual(html_node.value, "This is a text node")
 
 if __name__ == "__main__":
 	unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,10 +1,11 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 # types of inline text
 # since parsing markdown text to HTML text we need an intermediate representation
 
 class TextType(Enum):
-        PLAIN  = "text"
+        TEXT  = "text"
         BOLD = "bold"
         ITALIC = "italic"
         CODE = "code"
@@ -30,5 +31,32 @@ class TextNode:
 	def __repr__(self):
 		return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
+# handle each type of the TextType enum and return new LeafNode object
+def text_node_to_html_node(text_node):
 
+	# return a LeafNode with no tag, just a raw text value
+	if text_node.text_type == TextType.TEXT:
+		return LeafNode(None, text_node.text)
 
+	# return a LeafNode with a "b" tag and the text
+	elif text_node.text_type == TextType.BOLD:
+		return LeafNode('b', text_node.text)
+	
+	# return LeafNode with "i" tag, text
+	elif text_node.text_type == TextType.ITALIC:
+		return LeafNode('i', text_node.text)
+	
+	# return LeafNode with "code" tag, text
+	elif text_node.text_type == TextType.CODE:
+		return LeafNode('code', text_node.text)
+
+	# "a" tag, anchor text, and "href" prop
+	elif text_node.text_type == TextType.LINK:
+		return LeafNode('a', text_node.text, {'href':text_node.url})
+	
+	# "img" tag, empty string value, "src" and "alt" props ("src" is the image URL, "alt" is the alt text)
+	elif text_node.text_type == TextType.IMAGE:
+		return LeafNode('img', '', {'src':text_node.url, 'alt':text_node.text})
+
+	else:
+		raise Exception()
